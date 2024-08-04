f8590b1e3ee7e1edda0f65dd363b8f89f59ef21c
samehan <samehansonwane@gmail.com>
2024-08-04 14:22:03 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,19 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Samehan Sonwane on 08/04/24.
+//
+
+
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
